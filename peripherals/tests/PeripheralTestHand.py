13b862efdffeaec4f703b5366049990079a099be
Noah Albers <noah.albers@artandtech.space>
2023-05-10 11:50:46 +0200

    - Change hand-test to wait for the prime switch to be enabled before sending the hand

--- peripherals/tests/PeripheralTestHand.py
+++ peripherals/tests/PeripheralTestHand.py
@@ -1,11 +1,18 @@
 from core.CoreData import CoreData
-import asyncio as aio
 from loggingsystem.Logger import Logger
-
+from utils.ButtonListeners import ButtonListener
+from peripherals.PeripheralConfig import Inputs
 
 # Tests the hand
 async def on_hand_test(logger: Logger, core: CoreData):
-    logger.debug("on_hand_test", "starting test")
+    logger.debug("on_hand_test", "=== starting test, will now wait for open prime-switch")
+
+    await ButtonListener()\
+        .wait_for_change(Inputs.Buttons.GPIO_PRIME_SWITCH)\
+        .then()
+
+    logger.debug("on_hand_test","prime-switch found as open, start moving")
+
     await core.hand.move_outside()
     await core.hand.move_inside()
-    logger.debug("on_hand_test", "ending test")
+    logger.debug("on_hand_test", "=== ending test")
