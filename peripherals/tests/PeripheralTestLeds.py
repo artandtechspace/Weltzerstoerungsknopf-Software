99696bd81b6b02e25b0dd6ef52fc95690d8a01bf
Noah Albers <noah.albers@artandtech.space>
2023-05-10 11:51:36 +0200

    - Fixed led test not pushing state error

--- peripherals/tests/PeripheralTestLeds.py
+++ peripherals/tests/PeripheralTestLeds.py
@@ -7,20 +7,25 @@ import neopixel
 async def on_test_animation(pxls: neopixel.NeoPixel):
     # Plays R-G-B-W for all pixels
     pxls.fill((255, 0, 0))
+    pxls.show()
     await aio.sleep(1)
     pxls.fill((0, 255, 0))
+    pxls.show()
     await aio.sleep(1)
     pxls.fill((0, 0, 255))
+    pxls.show()
     await aio.sleep(1)
     pxls.fill((255, 255, 255))
+    pxls.show()
     await aio.sleep(1)
     pxls.fill((0, 0, 0))
+    pxls.show()
 
 
 # Tests the leds
 async def on_leds_test(logger: Logger, core: CoreData):
-    logger.debug("on_leds_test", "starting test")
+    logger.debug("on_leds_test", "=== starting test")
     # Starts the animation and waits for it's finishing
     await core.animations.start_animation(on_test_animation)
     await aio.sleep(4.5)
-    logger.debug("on_leds_test", "ending test")
+    logger.debug("on_leds_test", "=== ending test")
